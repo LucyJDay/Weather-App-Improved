8e00d5c29442a299918f7cbbd62f2b283f6b5f3d
LucyJDay <38351327+LucyJDay@users.noreply.github.com>
2024-08-23 11:23:40 +0100

    Update Temperature & City search function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
+function updateWeather(response) {
+  let currentTempElement = document.querySelector("#current-temp");
+  let currentTemp = response.data.temperature.current;
+  let cityElement = document.querySelector("#city-name");
+  cityElement.innerHTML = response.data.city;
+  currentTempElement.innerHTML = Math.round(currentTemp);
+}
+
+function searchCity(city) {
+  let apiKey = "a47eoa5f93f1482be2363ad1451fc0at";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#city-form-input");
-  let cityElement = document.querySelector("#city-name");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#city-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Barcelona");
